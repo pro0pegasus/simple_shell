14d6b1bc417a942123ececa6f5e6001d3599c02d
Aminetoumi <toumibusiness0@gmail.com>
2023-05-24 07:13:24 -0700

    update

---
+++ str_function4.c
@@ -0,0 +1,95 @@
+#include "main.h"
+
+/**
+ * **strtow0 - a string split into words we ignore repeats
+ * @stri: inp string
+ * @del: deli string
+ *
+ * Return: NULL if fails or string array pointer
+ */
+char **strtow0(char *stri, char *del)
+{
+	int a, b, c, d, nwords = 0;
+	char **st;
+
+	if (stri == NULL || stri[0] == 0)
+		return (NULL);
+	if (!del)
+		del = " ";
+	for (a = 0; stri[a] != '\0'; a++)
+		if (!is_deli(str[a], del) && (is_deli(stri[a + 1], del) || !str[a + 1]))
+			nwords++;
+
+	if (nwords == 0)
+		return (NULL);
+	st = malloc((1 + nwords) * sizeof(char *));
+	if (!str)
+		return (NULL);
+	for (a = 0, b = 0; b < nwords; b++)
+	{
+		while (is_deli(stri[a], del))
+			a++;
+		c = 0;
+		while (!is_deli(stri[a + c], del) && stri[a + c])
+			c++;
+		st[b] = malloc((c + 1) * sizeof(char));
+		if (!st[b])
+		{
+			for (c = 0; c < b; c++)
+				free(st[c]);
+			free(st);
+			return (NULL);
+		}
+		for (d = 0; d < c; d++)
+			st[b][d] = stri[a++];
+		st[b][d] = 0;
+	}
+	st[b] = NULL;
+	return (st);
+}
+
+/**
+ * **strtow1 - a string split into words
+ * @stri: inp string
+ * @del: deli string
+ *
+ * Return: NULL if fails or string array pointer
+ */
+char **strtow1(char *stri, char del)
+{
+	int a, b, c, d, nwords = 0;
+	char **st;
+
+	if (stri == NULL || stri[0] == 0)
+		return (NULL);
+	for (a = 0; stri[a] != '\0'; a++)
+		if ((stri[a] != del && stri[a + 1] == del)
+				|| (stri[a] != del && !stri[a + 1]) || stri[a + 1] == del)
+		       nwords++;
+	if (nwords == 0)
+		return (NULL);
+	st = malloc((1 + nwords) * sizeof(char *));
+	if (!st)
+		return (NULL);
+	for (a = 0 b = 0; b < nwords; b++)
+	{
+		while (stri[a] == del && stri[a] != del)
+			a++;
+		c = 0;
+		while (stri[a + c] != del && stri[a + c] && stri[a + c] != del)
+			c++;
+		st[b] = malloc((c + 1) * sizeof(char));
+		if (!st[b])
+		{
+			for (c = 0; c < b; c++)
+				free(st[c]);
+			free(st);
+			return (NULL);
+		}
+		for (d = 0; d < c; d++)
+			s[b][d] = stri[a++];
+		s[b][d] = 0;
+	}
+	s[b] = NULL;
+	return (st);
+}
